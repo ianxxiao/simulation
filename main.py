22f7242388ae6afe187c9831f9b3c431704a2f56
ian-xxiao <ian.xxiao@gmail.com>
2020-02-04 16:50:44 -0500

    - created class for advertising example

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import simpy
-
-def car(env):
-    while True:
-        print('Start parking at %d' % env.now)
-        parking_duration = 5
-        yield env.timeout(parking_duration)
-
-        print('Start driving at %d' % env.now)
-        trip_duration = 2
-        yield env.timeout(trip_duration)
+from sims import AdSim
 
 
 def main():
-    env = simpy.Environment()
-    env.process(car(env))
-    env.run(until=15)
+    ad = AdSim()
+    ad.run_sim()
+    ad._set_budget(50000)
+    ad.run_sim()
 
 
 if __name__ == '__main__':
