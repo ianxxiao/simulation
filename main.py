0449abbd0c4f576c625b2a3046ebe672d1c76ea4
ian-xxiao <ian.xxiao@gmail.com>
2020-02-04 17:13:51 -0500

    - added streamlit interface; parameterized ad budget

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import simpy
 from sims import AdSim
-
+import streamlit as st
 
 def main():
-    ad = AdSim()
-    ad.run_sim()
-    ad._set_budget(50000)
-    ad.run_sim()
+
+    # set up layout
+    st.title("Simulator")
+    ad_budget = st.slider(label='Pick an Advertising Budget',
+                          max_value=50000,
+                          min_value=20000,
+                          step=5000)
+
+    ad = AdSim(ad_budget)
+    sales = ad.run_sim()
+
+    st.text(f'expected sales ${sales:.2f}')
 
 
 if __name__ == '__main__':
--- sims.py
+++ sims.py
@@ -1,31 +1,35 @@
 import math
+
+
 class AdSim():
 
-    def __init__(self):
+    def __init__(self, ad_budget):
         print("setting up advertising class")
-        self.ad_budget = 40000
+        self.ad_budget = ad_budget
         self.price = 36.0
         self.cost = 31.0
         self.seasonal_var = {"Q1": {
-                        "unit_sold_adj": 0.9,
-                        "sales_overhead": 8000},
-            "Q2": {
-            "unit_sold_adj": 1.1,
+            "unit_sold_adj": 0.9,
             "sales_overhead": 8000},
+            "Q2": {
+                "unit_sold_adj": 1.1,
+                "sales_overhead": 8000},
             "Q3": {
                 "unit_sold_adj": 0.8,
                 "sales_overhead": 9000},
             "Q4": {
                 "unit_sold_adj": 1.2,
                 "sales_overhead": 9000}
-                        }
+        }
 
     def run_sim(self):
 
-        unit_sales = 35 * self.seasonal_var['Q1']['unit_sold_adj'] * math.sqrt(3000 + self.ad_budget)
+        sales = 35 * self.seasonal_var['Q1']['unit_sold_adj'] * math.sqrt(3000 + self.ad_budget)
 
         print("running simulation ...")
-        print(f'expected unit sales ${unit_sales:.2f}')
+        print(f'expected sales ${sales:.2f}')
+
+        return sales
 
     def _set_budget(self, new_budget):
         print(f'setting new advertising budget to ${new_budget}')
